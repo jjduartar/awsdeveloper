5adf2941fb5f960e62204a7e932877824832fb60
Josue Duarte <duartar@hotmail.com>
2021-08-08 14:36:50 -0500

    update deleteTodo

--- src/App.js
+++ src/App.js
@@ -36,9 +36,9 @@ function App() {
     setFormData(initialFormState)
   }
 
-  const deleteNote = async ({ element }) => {
-    await API.graphql({ query: deleteTodo, variables: { input: element.id } })
-    const newNotesArray = notes.filter(note => note.id !== element.id);
+  const deleteNote = async ({ id }) => {
+    await API.graphql({ query: deleteTodo, variables: { input: {id} } })
+    const newNotesArray = notes.filter(note => note.id !== id);
     setFormData(initialFormState)
     setNotes(newNotesArray)
   }
