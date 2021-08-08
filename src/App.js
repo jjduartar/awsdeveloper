61ea5b28cc1b8a84fc3464134e18accfbc7b87fc
Josue Duarte <duartar@hotmail.com>
2021-08-08 13:06:58 -0500

    Add graphQL

--- src/App.js
+++ src/App.js
@@ -1,26 +1,88 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API, Storage } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listTodos } from './graphql/queries';
+import { createTodo, deleteTodo } from './graphql/mutations';
 
 function App() {
+  const initialFormState = { name: "", description: "", image: "" };
+
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  // componentMount();
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const graphQL = await API.graphql({ query: listTodos }); // Fetch data from graphql
+    const noteItems = graphQL.data.listTodos.items;
+    await Promise.all(noteItems.map(async element => {
+      if (element.image) {
+        const image = await Storage.get(element.image);
+        element.image = image;
+      }
+      return element;
+    }));
+    setNotes(noteItems)
+
+  };
+
+  const createNote = async () => {
+    await API.graphql({ query: createTodo, variables: { input: formData } })
+    setNotes([...notes, formData]); //array.push(formData)
+    setFormData(initialFormState)
+  }
+
+  const deleteNote = async ({ id }) => {
+    await API.graphql({ query: { deleteTodo, variables: { input: id } } })
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setFormData(initialFormState)
+    setNotes(newNotesArray)
+  }
+
+  const onFileChanged = async (event) => {
+    const file = event.target.files[0];
+    setFormData({ ...formData, image: file.name })
+    await Storage.put(file.name, file);
+    fetchData();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <AmplifySignOut/>
-    </div>
+      <h1>Mis notas</h1>
+
+      <input
+        onChange={event => setFormData({ ...formData, name: event.target.value })}
+        placeholder={"Nombre de la nota"}
+        value={formData.name}
+      />
+
+      <input
+        onChange={event => setFormData({ ...formData, description: event.target.value })}
+        placeholder={"Descripcion de la nota"}
+        value={formData.description}
+      />
+
+      <input
+        onChange={onFileChanged}
+        type="file"
+      />
+
+      <div style={{ marginBottom: 30 }}>
+        {notes.map(element => {
+          return (<div style={{ border: '1px solid black' }}>
+            <h2>{element.name}</h2>
+            <h3>{element.name}</h3>
+            <img src={element.image} width="200px"></img>
+            <button onClick={() => deleteNote(element)}>Borrar nota</button>
+          </div>)
+        })}
+      </div>
+      <button onClick={createNote}>Crear nota</button>
+      <AmplifySignOut />
+    </div >
   );
 }
 
--- src/aws-exports.js
+++ src/aws-exports.js
@@ -7,7 +7,11 @@ const awsmobile = {
     "aws_cognito_region": "us-east-1",
     "aws_user_pools_id": "us-east-1_3R6pwXLQK",
     "aws_user_pools_web_client_id": "ug7v3h28cu0aqh643q634o2q5",
-    "oauth": {}
+    "oauth": {},
+    "aws_appsync_graphqlEndpoint": "https://zfsswqxx3favxgf5jgtey76ame.appsync-api.us-east-1.amazonaws.com/graphql",
+    "aws_appsync_region": "us-east-1",
+    "aws_appsync_authenticationType": "API_KEY",
+    "aws_appsync_apiKey": "da2-zptvao4jqbb2xnjwoibdoeswom"
 };
 
 
